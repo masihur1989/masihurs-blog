99d197e3bac80102d15f3bbe6261aa6c78950f06
masihur <masihur.maruf@talentinc.com>
2019-10-24 09:40:57 -0400

    server foundation

---
+++ server/go.mod
@@ -0,0 +1,8 @@
+module github.com/masihur1989/masihurs-blog/server
+
+go 1.12
+
+require (
+	github.com/gin-gonic/contrib v0.0.0-20190923054218-35076c1b2bea
+	github.com/gin-gonic/gin v1.4.0
+)
--- server/main.go
+++ server/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	// set the router with default one that comes with gin
+	router := gin.Default()
+	// server the static files
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+	// setup route group
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/ping", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+	// return the router
+	return router
+}
 
 func main() {
-	fmt.Println("main func for my blog!")
+	r := setupRouter()
+	// start and run the servier
+	_ = r.Run(":3000")
 }
